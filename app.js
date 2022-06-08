48f5288a74ff62c2475bf13d7783ceea0a5b8264
bartolomeow <carlos@carlospaque.com>
2022-06-08 10:23:13 +0200

    Installed and implemented cstojson module

---
+++ app.js
@@ -0,0 +1,23 @@
+// require csvtojson module
+const CSVToJSON = require('csvtojson');
+const fs = require('fs');
+
+// convert users.csv file to JSON array
+(async () => {
+    try {
+        const users = await CSVToJSON().fromFile('users.csv');
+
+        // log the JSON array
+        console.log(users);
+
+        fs.writeFile('users.json', JSON.stringify(users, null, 4), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON array is saved.");
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+})();
